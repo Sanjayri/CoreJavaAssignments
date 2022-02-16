a22e7f37213129d284280849648c173b6174a0e5
Sanjayri <98882617+Sanjayri@users.noreply.github.com>
2022-02-16 18:04:48 +0530

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,17 @@
+package BasicProgram;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=2;i<=50;i=i+2)
+		{
+			if(i%2==0)
+				System.out.println(i);
+		
+		}
+
+	}
+
+}
